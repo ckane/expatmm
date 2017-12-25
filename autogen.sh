fa7814ddf7cc7290df25205d5291cc6e2a961881
Coleman Kane <ckane@colemankane.org>
2017-12-25 11:10:45 -0500

    Don't distribute old ltmain.sh with package

--- autogen.sh
+++ autogen.sh
@@ -6,6 +6,7 @@ AUTOMAKE=`which automake 2>/dev/null`
 AUTOHEADER=`which autoheader 2>/dev/null`
 AUTOCONF=`which autoconf 2>/dev/null`
 
+libtoolize --copy --install --force
 $ACLOCAL
 $AUTOHEADER
 $AUTOMAKE --foreign --add-missing --copy
